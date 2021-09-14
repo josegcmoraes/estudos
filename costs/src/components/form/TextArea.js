970c925a215f892f1daf40a0dc2fa031025ea44f
josegcmoraes <josegcmoraes@gmail.com>
2021-09-14 10:12:03 -0300

    adição do componente textarea e botão de data

--- costs/src/components/form/TextArea.js
+++ costs/src/components/form/TextArea.js
@@ -1,18 +1,19 @@
-import styles from './Input.module.css'
+import styles from './TextArea.module.css'
 
-export function Input({ text, type, name, placeholder, value, handleOnChange, required }) {
+export function TextArea({ text, name, placeholder, cols, rows, value, handleOnChange, required }) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <input
-                type={type}
+            <textarea
                 name={name}
                 id={name}
                 placeholder={placeholder}
+                cols={cols}
+                rows={rows}
                 value={value}
                 onChange={handleOnChange}
                 required={required === "true"}
-            />
+            ></textarea>
         </div>
     )
 }
--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -3,6 +3,7 @@ import styles from './ProjectForm.module.css'
 import { Input } from '../form/Input'
 import { Select } from '../form/Select'
 import { SubmitButton } from '../form/SubmitButton'
+import { TextArea } from '../form/TextArea'
 
 export function ProjectForm({ btnText }) {
     return (
@@ -13,6 +14,7 @@ export function ProjectForm({ btnText }) {
                 type="text"
                 name="name"
                 placeholder="Insira o nome do projeto"
+                required="true"
             />
 
             <Input
@@ -20,11 +22,38 @@ export function ProjectForm({ btnText }) {
                 type="number"
                 name="budget"
                 placeholder="Insira o orçamento total"
+                required="true"
             />
 
             <Select
                 text="Selecione a categoria"
                 name="category_id"
+            />
+
+            <Input
+                text="Início do Projeto"
+                type="date"
+                name="initialDate"
+                // placeholder={new Intl.DateTimeFormat('pt-BR').format(new Date())}
+                // value="01/01/2021"
+                required="true"
+                min={new Date()}
+            />
+
+            <Input
+                text="Equipe Externa"
+                type="checkbox"
+                name="externalTime"
+                required="false"
+            />
+
+            <TextArea
+                text="Descrição do Projeto"
+                name="description"
+                placeholder="Digite a descrição do projeto"
+                cols="15"
+                rows="5"
+                required="false"
             />
 
             <SubmitButton
